7a79f915d68f7d5217152c408d333eb796d9760b
ismail baoud <ibaoud46@egmail.com>
2025-10-10 20:04:46 +0100

    Add Specialist.java

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/models/person/Specialist.java
@@ -0,0 +1,61 @@
+package com.medicale.consultation.consultationmedicale.models.person;
+
+import com.medicale.consultation.consultationmedicale.enums.Gender;
+import com.medicale.consultation.consultationmedicale.enums.Role;
+import com.medicale.consultation.consultationmedicale.enums.Speciality;
+import com.medicale.consultation.consultationmedicale.models.consultation.Request;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@DiscriminatorValue("specialists")
+public class Specialist extends Person{
+    @Column(nullable = false, unique = true, name = "numero_RPPS")
+    private String numeroRPPS;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Speciality speciality;
+
+    @Column(nullable = false)
+    private double fee;
+
+    @OneToMany(mappedBy = "specialist", cascade = CascadeType.ALL)
+    private List<Request> requests = new ArrayList<>();
+
+    public Specialist(int id, String firstName, String lastName, String username, String password, String phone, Gender gender, LocalDate createdAt, Role role, String numeroRPPS, Speciality speciality, double fee) {
+        super(id, firstName, lastName, username, password, phone, gender, createdAt, role);
+        this.numeroRPPS = numeroRPPS;
+        this.speciality = speciality;
+        this.fee = fee;
+    }
+
+    public Specialist() {}
+
+    public String getNumeroRPPS() {
+        return numeroRPPS;
+    }
+
+    public void setNumeroRPPS(String numeroRPPS) {
+        this.numeroRPPS = numeroRPPS;
+    }
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}
