c889c59051a16690e9aa89ffe22b65ca1ee6f15e
ismail baoud <ibaoud46@egmail.com>
2025-10-10 20:04:46 +0100

    Add PatientServlet.java

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/controller/PatientServlet.java
@@ -0,0 +1,107 @@
+package com.medicale.consultation.consultationmedicale.controller;
+
+import com.medicale.consultation.consultationmedicale.enums.ConsultationStatus;
+import com.medicale.consultation.consultationmedicale.enums.Role;
+import com.medicale.consultation.consultationmedicale.models.MedicaleFile;
+import com.medicale.consultation.consultationmedicale.models.Ticket;
+import com.medicale.consultation.consultationmedicale.models.consultation.Consultation;
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import com.medicale.consultation.consultationmedicale.models.person.Person;
+import com.medicale.consultation.consultationmedicale.service.ConsultationService;
+import com.medicale.consultation.consultationmedicale.service.MedicalFileSevice;
+import com.medicale.consultation.consultationmedicale.service.PersonService;
+import com.medicale.consultation.consultationmedicale.service.TicketService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@WebServlet("/savePatientOrVitals")
+public class PatientServlet extends BaseServlet {
+
+    public void findPatient(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PersonService personService = new PersonService();
+        try {
+            String fullName = req.getParameter("fullName");
+            String firstName = fullName.split(" ")[0];
+            String lastName = fullName.split(" ")[1];
+            Patient patient = personService.findAll().stream()
+                    .filter(p -> p instanceof Patient)
+                    .map(p -> (Patient) p)
+                    .filter(p -> p.getFirstName().trim().equalsIgnoreCase(firstName.trim())
+                            && p.getLastName().trim().equalsIgnoreCase(lastName.trim()))
+                    .findFirst()
+                    .orElse(null);
+
+            if(patient == null) {
+                view(req, resp, "patientAdd.jsp");
+                return;
+            }
+
+            req.setAttribute("patient", patient);
+            view(req, resp, "patientAdd.jsp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("error", "Erreur lors de l'enregistrement du patient !");
+            req.getRequestDispatcher("/dashboard").forward(req, resp);
+        }
+    }
+
+    public void createPatient(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PersonService personService = new PersonService();
+        MedicalFileSevice medicalFileSevice = new MedicalFileSevice();
+        TicketService ticketService = new TicketService();
+        ConsultationService consultationService = new ConsultationService();
+
+        try {
+            Patient patient = new Patient();
+            patient.setFirstName(req.getParameter("firstName"));
+            patient.setLastName(req.getParameter("lastName"));
+            patient.setDateOfBirth(LocalDate.parse(req.getParameter("dateOfBirth")));
+            patient.setEmail(req.getParameter("email"));
+            patient.setRole(Role.PATIENT);
+            patient.setPhone(req.getParameter("phone"));
+            patient.setCreatedAt(LocalDate.now());
+            patient.setWeight(Double.parseDouble(req.getParameter("weight")));
+            patient.setHeight(Double.parseDouble(req.getParameter("height")));
+            patient.setDossierNumber(req.getParameter("dossierNumber"));
+            patient.setPassword("1234");
+            System.out.println(patient.getWeight());
+            System.out.println(patient.getHeight());
+            personService.save(patient);
+
+
+            MedicaleFile medicaleFile = new MedicaleFile();
+            medicaleFile.setPatient(patient);
+            medicaleFile.setTemperature(Double.parseDouble(req.getParameter("temperature")));
+            medicaleFile.setPulse(Integer.parseInt(req.getParameter("pulse")));
+            medicaleFile.setBloodPresure(Integer.parseInt(req.getParameter("bloodPressure")));
+            medicaleFile.setRespiratoryRate(Integer.parseInt(req.getParameter("respiratoryRate")));
+            medicaleFile.setOxygenSaturation(Integer.parseInt(req.getParameter("oxygenSaturation")));
+            medicaleFile.setPain(Integer.parseInt(req.getParameter("pain")));
+            medicalFileSevice.save(medicaleFile);
+
+            Ticket ticket = new Ticket();
+            ticket.setCreatedAt(LocalDateTime.now());
+            ticket.setPatient(patient);
+            ticketService.save(ticket);
+
+            Consultation consultation = new Consultation();
+            consultation.setCreatedAt(LocalDateTime.now());
+            consultation.setConsultationStatus(ConsultationStatus.CREATED);
+            consultation.setMedicalFile(medicaleFile);
+            consultationService.save(consultation);
+
+            view(req, resp, "patientAdd.jsp");
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
