7d981cd70b95f03fcebb01b6a118f6452550cf48
ismail baoud <ibaoud46@egmail.com>
2025-10-10 20:04:46 +0100

    Add Ticket.java

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/models/Ticket.java
@@ -0,0 +1,53 @@
+package com.medicale.consultation.consultationmedicale.models;
+
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    public Ticket(int id, int order, LocalDateTime createdAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public Ticket() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+}
