097f7aa9fdfdbe2580be7b7275c0395ac4531499
ismail baoud <ibaoud46@egmail.com>
2025-10-10 20:04:47 +0100

    Add TicketService.java

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/service/TicketService.java
@@ -0,0 +1,27 @@
+package com.medicale.consultation.consultationmedicale.service;
+
+import com.medicale.consultation.consultationmedicale.models.MedicaleFile;
+import com.medicale.consultation.consultationmedicale.models.Ticket;
+import com.medicale.consultation.consultationmedicale.repositories.MedicalFileRepository;
+import com.medicale.consultation.consultationmedicale.repositories.TicketRepository;
+
+import java.util.List;
+
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService() {
+        this.ticketRepository = new TicketRepository();
+    }
+
+    /**
+     * Save a ticket (insert into DB)
+     */
+    public void save(Ticket ticket) {
+        ticketRepository.save(ticket);
+    }
+
+    public List<Ticket> findAll() {
+        return ticketRepository.findAll();
+    }
+}
