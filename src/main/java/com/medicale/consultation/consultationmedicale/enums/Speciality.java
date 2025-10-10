2f0b431a3ec7a5ffc01bd7f9bef0c7c1b23a1ba1
ismail baoud <ibaoud46@egmail.com>
2025-10-10 20:04:46 +0100

    Add Speciality.java

---
+++ src/main/java/com/medicale/consultation/consultationmedicale/enums/Speciality.java
@@ -0,0 +1,14 @@
+package com.medicale.consultation.consultationmedicale.enums;
+
+public enum Speciality {
+    ANESTHESIOLOGY,
+    CARDIOLOGY,
+    DERMATOLOGY,
+    EMERGENCY,
+    MEDICINE,
+    PSYCHIATRY,
+    RADIOLOGY,
+    FAMILY,
+    NEUROLOGY,
+    ONCOLOGY
+}
