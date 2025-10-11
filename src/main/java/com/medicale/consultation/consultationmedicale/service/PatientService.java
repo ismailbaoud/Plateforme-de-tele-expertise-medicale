5a1595cb2054c504ab33b5d4fc2f9f6e0dcf0368
ismail baoud <ibaoud46@egmail.com>
2025-10-11 16:23:13 +0100

    Update PatientService.java

--- src/main/java/com/medicale/consultation/consultationmedicale/service/PatientService.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/service/PatientService.java
@@ -1,4 +1,13 @@
 package com.medicale.consultation.consultationmedicale.service;
 
+import com.medicale.consultation.consultationmedicale.models.person.Patient;
+import com.medicale.consultation.consultationmedicale.repositories.PatientRepository;
+
+import java.util.List;
+
 public class PatientService {
+    PatientRepository patientRepository =  new PatientRepository();
+    public List<Patient> findAll() {
+        return patientRepository.findAll();
+    }
 }
