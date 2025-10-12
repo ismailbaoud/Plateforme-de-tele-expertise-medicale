0c8972d97d50ca0021db4301ebe1448c76afdc5b
ismail baoud <ibaoud46@egmail.com>
2025-10-12 20:12:56 +0100

    Update PersonService.java

--- src/main/java/com/medicale/consultation/consultationmedicale/service/PersonService.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/service/PersonService.java
@@ -13,39 +13,12 @@ public class PersonService {
         this.personRepository = new PersonRepository();
     }
 
-    /**
-     * Get all persons (Nurses, Generalists, Specialists)
-     */
     public List<Person> findAll() {
         return personRepository.findAll();
     }
 
-    /**
-     * Find person by username
-     */
-    public Person findByUsername(String username) {
-        return personRepository.findByUsername(username);
-    }
-
-    /**
-     * Save a person (insert into DB)
-     */
     public void save(Person person) {
         personRepository.save(person);
-    }
-
-    /**
-     * Find person by ID
-     */
-    public Person findById(Long id) {
-        return personRepository.findById(id);
-    }
-
-    /**
-     * Delete person by ID
-     */
-    public void delete(Long id) {
-        personRepository.delete(id);
     }
 
     public void update(Person person) {
