7d5bcf65a768f6329267387cd335246f19120154
ismail baoud <ibaoud46@egmail.com>
2025-10-12 20:12:56 +0100

    Update MedicalFileSevice.java

--- src/main/java/com/medicale/consultation/consultationmedicale/service/MedicalFileSevice.java
+++ src/main/java/com/medicale/consultation/consultationmedicale/service/MedicalFileSevice.java
@@ -12,46 +12,16 @@ public class MedicalFileSevice {
         public MedicalFileSevice() {
             this.medicalFileRepository = new MedicalFileRepository();
         }
-//
-//        /**
-//         * Get all persons (Nurses, Generalists, Specialists)
-//         */
-//        public List<Person> findAll() {
-//            return medicalFileRepository.findAll();
-//        }
-//
-//        /**
-//         * Find person by username
-//         */
-//public Person findByUsername(String username) {
-//    return medicalFileRepository.findByUsername(username);
-//}
+
         public List<MedicaleFile> findAll() {
             return medicalFileRepository.findAll();
         }
 
-        /**
-         * Save a person (insert into DB)
-         */
         public void save(MedicaleFile medicaleFile) {
             medicalFileRepository.save(medicaleFile);
         }
-//
-//        /**
-//         * Find person by ID
-//         */
-//        public Person findById(Long id) {
-//            return medicalFileRepository.findById(id);
-//        }
-//
-//        /**
-//         * Delete person by ID
-//         */
-//        public void delete(Long id) {
-//            medicalFileRepository.delete(id);
-//        }
 
-    public void update(MedicaleFile medicaleFile) {
+        public void update(MedicaleFile medicaleFile) {
             medicalFileRepository.update(medicaleFile);
     }
 
